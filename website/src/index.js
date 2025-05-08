e17fe9c4ceec69291d264fa49a9f6cf893e09e65
EWHM87 <ericwong070802@gmail.com>
2025-05-09 01:21:29 +0800

    website test 1

--- website/src/index.js
+++ website/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './framework/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = createRoot(document.getElementById('root'));
 console.log("Index rendering App now...");
